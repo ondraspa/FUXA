700457aaaeae94f8db6848afdbe249f7eabc0579
Greg Carter <greg9504@gmail.com>
2025-05-18 22:29:09 -0400

    use legacy mat dialog as master branch has been updated to Angular 15

--- client/src/app/device/device-enipselectdevice/device-enipselectdevice.component.ts
+++ client/src/app/device/device-enipselectdevice/device-enipselectdevice.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { HmiService } from '../../_services/hmi.service';
 import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
